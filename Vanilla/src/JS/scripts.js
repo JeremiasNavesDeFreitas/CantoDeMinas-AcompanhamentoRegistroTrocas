c8e662dc7906a0a917eab43c6efcbd1c82e10e0e
Jeremias <jnfbrofficial@gmail.com>
2023-05-25 15:54:00 -0300

    v.0.42 - Banco de Dados Local

---
+++ Vanilla/src/JS/scripts.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.querySelector('form');
+  
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Impede o envio padrão do formulário
+  
+      var formData = new FormData(form);
+  
+      // Enviar os dados para o arquivo PHP usando uma requisição Ajax
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', 'processar_formulario.php', true);
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          alert(xhr.responseText); // Exibe a resposta do servidor
+          form.reset(); // Limpa o formulário após o envio
+        }
+      };
+      xhr.send(formData);
+    });
+  });
